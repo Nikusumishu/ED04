6083c9d37985f8f7664626e450d3352362d1883d
Nikusumishu <99676996+Nikusumishu@users.noreply.github.com>
2022-02-14 19:51:33 +0100

    El JavaDoc se ha introducido en la clase CCuenta

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,73 +1,103 @@
 package cuentas;
 
+/**
+ * 
+ * @author David Lopez de la Muñoza
+ */
+
 public class CCuenta {
 
     /**
-     * @return the nombre
+     * Estan los metodos necesarios para trabajar con esta clase
+     * @return Nombre del titular
+     * 
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * @param nombre del titular de la cuenta
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * @return el numero de cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * @param añade una cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * @return el saldo disponible en la cuenta
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * @param añade saldo a la cuenta
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * @return obtiene el tipo de interes
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * @param tipoInterés añade el tipo de interes a la cuenta
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
-
+    
+    /**
+     * Nombre del titular de la Cuenta
+     */
     private String nombre;
+    /**
+     * La identificacion de la Cuenta
+     */
     private String cuenta;
+    /**
+     * El saldo disponible en la Cuenta
+     */
     private double saldo;
+    /**
+     * El interes que tiene en la Cuenta
+     */
     private double tipoInterés;
 
+    /**
+     * Constructor sin parametros
+     */
+    
     public CCuenta()
     {
     }
 
+    /**
+     * 
+     * @param nom Nombre del titular de la cuenta
+     * @param cue Datos de la cuenta
+     * @param sal Saldo de la cuenta
+     * @param tipo Interes de la cuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -75,11 +105,20 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * 
+     * @return Estado de la cuenta
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * 
+     * @param cantidad ingresa una cantidad a la cuenta
+     * @throws Exception Si la cantidad es negativa sale la excepcion
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -87,6 +126,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * 
+     * @param cantidad retira una cantidad de la cuenta
+     * @throws Exception Si no hay suficiente saldo o la cantidad a retirar es negativa, salta la excepcion
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
